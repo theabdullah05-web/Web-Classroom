6a3b9cfe61180c57cc3a6aa3ccbf072c3ae2b956
Abdulah Hussain <abdullahbhtty01@gmail.com>
2026-07-05 13:37:02 +0500

    Updated Calculator

--- Javascript/Calculator/app.js
+++ Javascript/Calculator/app.js
@@ -1,8 +1,37 @@
 let head=document.querySelector("h3");
-let input=document.querySelector("textarea");
-let btns=document.querySelectorAll("button");
+let clear=document.querySelector("#c")
+let input=document.querySelector("input");
+let btns=document.querySelectorAll(".btn");
+let equal=document.querySelector(".enter");
+let form=document.querySelector("form");
+
+function toEval(str){
+    return str.replace(/×/g,"*").replace(/÷/g, "/");
+}
+
 for(btn of btns){
     btn.addEventListener("click",function cl(){
-        console.log(this.value)
+        head.innerText=""
+        input.value+=this.innerText
     })
 }
+
+form.addEventListener("submit",function fo(e){
+    e.preventDefault();
+    if(input.value){
+    head.innerText=eval(toEval(input.value));
+    input.value=""}
+    })
+
+input.addEventListener("input",function eq(){;
+    head.innerText="";
+    if(input.value){
+    head.innerText=eval(toEval(input.value));
+    }
+
+});
+
+clear.addEventListener("click",function cle(){
+    input.value="";
+    head.innerText=""
+})
