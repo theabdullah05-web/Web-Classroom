6988abe39c2f45c85f53508a56c1f9c5e82ca978
Abdulah Hussain <abdullahbhtty01@gmail.com>
2026-07-06 11:02:06 +0500

    Updated Middleware Class

--- MIDDLEWARES/app.js
+++ MIDDLEWARES/app.js
@@ -1,12 +1,13 @@
 const express=require("express");
+const ExpressError = require("./ExpressError");
 const app=express();
 
-// //Logger
-// app.use((req,res,next)=>{
-//     req.time=new Date(Date.now()).toString();
-//     console.log(req.method,req.hostname,req.path,req.time)
-//     next();
-// });
+//Logger
+app.use((req,res,next)=>{
+    req.time=new Date(Date.now()).toString();
+    console.log(req.method,req.hostname,req.path,req.time)
+    next();
+});
 
 app.use("/random",(req,res,next)=>{
     console.log("I am for random only");
@@ -19,16 +20,20 @@ const checkToken=(req,res,next)=>{
     if(token =="giveAccess"){
         next()
     }else{
-        throw new Error("ACCESS DENIED!!!")
+        throw new ExpressError(401,"ACCESS DENIED!!!")
     }
 }
 
 app.get("/wrong",(req,res)=>{
-    abcd=abcd;
+    throw new ExpressError(400,"abcd is not defined!")
 })
 
 app.get("/api",checkToken,(req,res)=>{
     res.send("data")
+})
+
+app.get("/admin",(req,res)=>{
+    throw new ExpressError(403,"ACCESS DENIED")
 })
 
 app.get("/",async(req,res)=>{
@@ -39,9 +44,25 @@ app.get("/random",(req,res)=>{
     res.send("this is a random page")
 })
 
-app.use((req,res)=>{
-    res.status(404).send("Page not found")
+app.use((req,res,next)=>{
+    throw new ExpressError(404,"Page not Found")
 });
+
+app.use((err,req,res,next)=>{
+    console.log(err.name);
+
+    if(err.name==="ValidationError"){
+        err.status=400
+    }
+
+    if(err.name==="CastError"){
+        err.status=400;
+        err.message="InvalidID"
+    }
+
+    let{status=500,message="Some Error Occured"}=err;
+    res.status(status).send(message)
+})
 
 app.listen("8080",()=>{
     console.log("app is listening on port 8080")
